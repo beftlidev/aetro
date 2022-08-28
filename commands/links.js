ef9a7b4f2aacdfd11ea3ee31f75b170b39ff556d
Cheesey <77486662+Cheesey-dev@users.noreply.github.com>
2022-08-28 14:03:51 +0300

    Create links.js

---
+++ commands/links.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js')
+const {MessageEmbed, MessageActionRow, MessageButton} = require("discord.js") 
+const db = require('croxydb') 
+
+module.exports = {
+  en: {
+    name: "links", 
+    options: [], 
+    description: "🔗 Discards the bot\'s links.",
+  },
+    run: async (client, interaction) => {
+
+const embed = new Discord.MessageEmbed() 
+
+.setDescription("You can go to any link you want from below! \nWith the links below, you can add the bot to your Server, come to the bot's support server, vote for the bot and check my site. 😇")
+.setColor("GREEN")
+const row = new MessageActionRow()
+			.addComponents(
+ new Discord.MessageButton() 
+.setStyle("LINK") 
+.setLabel("My invitation link") 
+.setURL("https://discord.com/oauth2/authorize?client_id=1012986707328643072&scope=bot+applications.commands&permissions=2147483656"), 
+new Discord.MessageButton() 
+.setStyle("LINK") 
+.setLabel("My support server") 
+.setURL(""), 
+new Discord.MessageButton() 
+.setStyle("LINK") 
+.setLabel("My voting link") 
+.setURL("https://top.gg/bot/1012986707328643072/vote"), 
+new Discord.MessageButton() 
+.setStyle("LINK") 
+.setLabel("Web Site") 
+.setURL("https://aetro.xyz/") 
+); 
+await interaction.reply({embeds: [embed], components: [row]})  
+
+} 
+}
