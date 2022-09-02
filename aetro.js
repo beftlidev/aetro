62998db085c30add07b5bf01ea99e81c9d717466
Cheesey <77486662+Cheesey-dev@users.noreply.github.com>
2022-09-02 22:52:31 +0300

    Update aetro.js

--- aetro.js
+++ aetro.js
@@ -636,7 +636,114 @@ interaction.update({embeds: [embed]})
 // üye giriş çıkış ekle
 
 
+client.on('guildMemberAdd', async(interaction) => {
 
+if(await client.channels.fetch(`welcome_goodbye_${interaction.guild.id}`)) {
+
+      const canvas = Canvas.createCanvas(1772, 633);
+      const ctx = canvas.getContext('2d');
+      const background = await Canvas.loadImage(`https://media.discordapp.net/attachments/1012992205700481025/1015262785342550058/welcome_1.png`);
+      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = '#f2f2f2';
+      ctx.strokeRect(0, 0, canvas.width, canvas.height);
+      var textString3 = `${interaction.user.username}`;
+      if (textString3.length >= 14) {
+        ctx.font = 'bold 100px Genta';
+        ctx.fillStyle = '#f2f2f2';
+        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
+      }
+      else {
+        ctx.font = 'bold 100px Genta';
+        ctx.fillStyle = '#f2f2f2';
+        ctx.fillText(textString3, 720, canvas.height / 2 + 5);
+      }
+      var textString2 = `#${interaction.user.discriminator}`;
+      ctx.font = 'bold 40px Genta';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString2, 730, canvas.height / 2 + 58);
+      var textString4 = `${interaction.guild.memberCount}th Member`;
+      ctx.font = 'bold 60px Genta';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString4, 750, canvas.height / 2 + 125);
+      var textString4 = `${interaction.guild.name}`;
+      ctx.font = 'bold 60px Genta';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString4, 700, canvas.height / 2 - 150);
+      ctx.beginPath();
+      ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);
+      ctx.closePath();
+      ctx.clip();
+      const avatar = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
+      ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
+      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+      const welcomeembed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTimestamp() 
+        .setDescription(`${await client.emoji.fetch(`newmember`)} ${interaction.user}, welcome to the ${interaction.guild.name} server!`)
+        .setImage("attachment://welcome-image.png")
+      const channel = client.channel.cache.get(await client.channels.fetch(`welcome_goodbye_${interaction.guild.id}`))
+      channel.send({embeds: [welcomeembed, files: [attachment]]});
+
+} else {
+return;
+}
+
+})
+
+
+client.on('guildMemberRemove', async(interaction) => {
+
+if(await client.channels.fetch(`welcome_goodbye_${interaction.guild.id}`)) {
+
+      const canvas = Canvas.createCanvas(1772, 633);
+      const ctx = canvas.getContext('2d');
+      const background = await Canvas.loadImage(`https://media.discordapp.net/attachments/1012992205700481025/1015262785342550058/welcome_1.png`);
+      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = '#f2f2f2';
+      ctx.strokeRect(0, 0, canvas.width, canvas.height);
+      var textString3 = `${interaction.user.username}`;
+      if (textString3.length >= 14) {
+        ctx.font = 'bold 100px Genta';
+        ctx.fillStyle = '#f2f2f2';
+        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
+      }
+      else {
+        ctx.font = 'bold 100px Genta';
+        ctx.fillStyle = '#f2f2f2';
+        ctx.fillText(textString3, 720, canvas.height / 2 + 5);
+      }
+      var textString2 = `#${interaction.user.discriminator}`;
+      ctx.font = 'bold 40px Genta';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString2, 730, canvas.height / 2 + 58);
+      var textString4 = `${interaction.guild.memberCount} Member`;
+      ctx.font = 'bold 60px Genta';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString4, 750, canvas.height / 2 + 125);
+      var textString4 = `${interaction.guild.name}`;
+      ctx.font = 'bold 60px Genta';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString4, 700, canvas.height / 2 - 150);
+      ctx.beginPath();
+      ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);
+      ctx.closePath();
+      ctx.clip();
+      const avatar = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
+      ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
+      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+      const welcomeembed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTimestamp() 
+        .setDescription(`${await client.emoji.fetch(`no`)} **${interaction.user.username}#${interaction.user.discriminator}** left the server, see you`)
+        .setImage("attachment://welcome-image.png")
+      const channel = client.channel.cache.get(await client.channels.fetch(`welcome_goodbye_${interaction.guild.id}`))
+      channel.send({embeds: [welcomeembed, files: [attachment]]});
+
+} else {
+return;
+}
+
+})
 
 
 // level ekle
