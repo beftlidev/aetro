a7694571bc5ccfdff6a41024fd293fa0090e30b4
Cheesey <77486662+Cheesey-dev@users.noreply.github.com>
2022-09-03 10:26:05 +0300

    Update moderation.js

--- commands/moderation.js
+++ commands/moderation.js
@@ -9,7 +9,7 @@ module.exports = {
     description: "⚒️ It allows you to use authorized commands.",
   },
     run: async (client, interaction) => {
-
+await interaction.deferReply()
 if(!interaction.member.permissions.has('ADMINISTRATOR')){
             return interaction.reply({
                 content: `${await client.emoji.fetch(`no`)} You must have permissions to administrator to moderation commands..`,
@@ -34,13 +34,13 @@ const row = new MessageActionRow().addComponents(
                 .setEmoji(""),
 )
 
-const message = await interaction.reply({embeds: [embed], components: [row]})
+const message = await interaction.channel.send({embeds: [embed], components: [row]})
 
 await client.moderation.set(`moderation_user_${message.id}`, `${interaction.user.id}`)
 
     setTimeout(async () => {
 await client.moderation.delete(`moderation_user_${message.id}`)
-message.edit({content: `${await client.emoji.fetch(`no`)} This command has expired.`, embeds: [], components: []})
+message.delete()
 }, 300000)
 
 } 
